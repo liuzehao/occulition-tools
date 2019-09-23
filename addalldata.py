89072e21af9d85cc3e7f2e4119cfdeabda758495
刘泽豪 <liuzehao139@163.com>
2019-09-23 21:16:20 +0800

    Update addalldata.py

--- addalldata.py
+++ addalldata.py
@@ -1,6 +1,8 @@
 # coding=UTF-8
 import os
 import shutil
+import operator
+import cv2
 #1.找到外部文件夹
 #2.计算图片数量
 #3.顺序命名，封装成函数来调用 输入图片路径
@@ -14,36 +16,29 @@ def get_all_files(bg_path):
             files.extend(get_all_files(os.path.join(bg_path, f)))
     files.sort(key=lambda x: int(x[-9:-4]))#排序从小到大
     return files
-path=["./output1","./output2","./output3","./output4","./output5","./output6","./output7","./output8","./output9","./output10","./output11","./output12","./output13"]
-categorys_all = ['ape','can','cat','driller','duck','eggbox','glue','holepuncher']
-outpath='../outdata/'
-name_flag=0
-for i in path:
-    for t in categorys_all:
-        img_path=i+"/"+t+"/image"
-        xml_path=i+"/"+t+"/xml"
+path=["./OcclusionChallengeICCV2015"]
 
-        temp=name_flag
-        if os.path.exists(img_path) and os.path.exists(xml_path):
-            num_img=get_all_files(img_path)
-            xml_img=get_all_files(xml_path)
-        else:
-            continue
-        if cmp(num_img,xml_img):
-            for t in num_img:
-                name = '%06d' % int(temp)
-                pic_name=outpath+'image/'+name+'.jpg'
-                print(t)
-                shutil.copy(t,pic_name)
-                temp+=1
+outpath='./outdatatest/'
+img_path="./OcclusionChallengeICCV2015/image"
+xml_path="./OcclusionChallengeICCV2015/xml"
 
-            temp=name_flag
-            
-            
-            for z in xml_img:
-                name = '%06d' % int(temp)
-                xml_name=outpath+'xml/'+name+'.xml'
-                print(z)
-                shutil.copy(z,xml_name)
-                temp+=1
-            name_flag=temp
+temp=0
+if os.path.exists(img_path) and os.path.exists(xml_path):
+    num_img=get_all_files(img_path)
+    xml_img=get_all_files(xml_path)
+else:
+    exit("no more")
+for i in range(len(xml_img)):
+    if operator.eq(xml_img[i][-9:-4],num_img[i][-9:-4]):
+
+        name = '%06d' % int(temp)
+        pic_name=outpath+'ImageSets/'+name+'.jpg'
+
+            #png转jpg
+        p=cv2.imread(num_img[i])
+        cv2.imwrite(pic_name,p)
+       
+        xml_name=outpath+'Annotations/'+name+'.xml'
+
+        shutil.copy(xml_img[i],xml_name)
+        temp+=1
